7674f8201f2628bd20770bf749027d22dcbb790a
martijn <m97devries@gmail.com>
2023-06-22 19:51:11 +0200

    script for roman to in leetcode now works (first c++ challenge!!!).

--- roman_to_int.cpp
+++ roman_to_int.cpp
@@ -11,63 +11,124 @@
 //Add some convenient functions to the scope:
 using namespace std;
 
-//class used to represent a roman number:
 class Roman_number {
 public:
-  //constructor
-  Roman_number(string roman_number) {
-    this->roman_number = roman_number; 
-  }
 
-  //Define conversion table from roman to normal integers:
-  //I think that special cases can already be added here??
-  map<string, int> conversion_table = {
-    {"I", 1},
-    {"V", 5},
-    {"X", 10},
-    {"L", 50},
-    {"C", 100},
-    {"D", 500},
-    {"M", 1000}
+  //Constructor:
+  string input_number;
+  Roman_number(string input_number) {
+    this->input_number = input_number;
   };
 
   //Function to determine length of roman number:
-  int get_string_length() {
+  int get_length() {
     int length = 0;
-    length = roman_number.size();
+    length = input_number.size();
     return length;
   };
 
-  //Function to convert the roman number to corresponding integers:
-  int romanToInt() {
-    //Declare integer to store output:
-    int roman_int = 0;
-    
-    //First the length is determined:
-    int length = get_string_length();
+  //Function to convert roman char to int:
+  int get_roman_int(char roman_character) {
+    int converted_number = 0;
 
-    //If lengt is one we just convert the number:
-    if (length == 1) {
-      roman_int = conversion_table[roman_number];
+    //Conversion table:
+    map<char, int> table = {
+      {'I', 1},
+      {'V', 5},
+      {'X', 10},
+      {'L', 50},
+      {'C', 100},
+      {'D', 500},
+      {'M', 1000}
     };
-
-    //If not we have to calculate the integer:
-    return roman_int;
+    
+    converted_number = table[roman_character];
+    return converted_number;
   };
 
-private:
-  string roman_number;
-};
+  //Function to return char from string at certain positions:
+  char get_character(int position) {
+    char outcharacter = ' ';
 
+    outcharacter = input_number[position];
+    
+    return outcharacter;
+  };
+
+  //Function that converts roman number to integer:
+  int roman_to_int() {
+    int converted_number = 0;
+
+    //First we determine the length of the input number:
+    int input_length = 0;
+    input_length = get_length();
+
+    //If the length is one we just convert the input number:
+    if (input_length == 1) {
+      converted_number = get_roman_int(input_number[0]);
+    }
+    else {
+      //if not we have to perform the conversion:
+      for (int i = 1; i <= input_length; ++i) {
+	char current_character = get_character(i);
+	char previous_character = get_character(i - 1);
+
+	int current_character_int = get_roman_int(current_character);
+	int previous_character_int = get_roman_int(previous_character);
+	
+	//Now we check for edge cases:
+
+	//First 9 and 4:
+	if (previous_character == 'I') {
+	  if (current_character == 'V' || current_character == 'X') {
+	    ++i;
+	    converted_number += (current_character_int -
+				 previous_character_int);
+	    continue;
+	  }
+	};
+
+	//Then 40 and 90:
+	if (previous_character == 'X') {
+	  if (current_character == 'L' || current_character == 'C') {
+	    ++i;
+	    converted_number += (current_character_int -
+				 previous_character_int);
+	    continue;
+	  }
+	};
+
+	//Finally 400 and 900:
+	if (previous_character == 'C') {
+	  if (current_character == 'D' || current_character == 'M') {
+	    ++i;
+	    converted_number += (current_character_int -
+				 previous_character_int);
+	    continue;
+	  }
+	};
+
+	//If not an edge case do something ...
+	converted_number += (current_character_int + previous_character_int);
+	++i;
+      };
+    };
+    
+    return converted_number;
+    
+  };
+};
 
 int main() {
   //User can declare a roman numeral that will be converted:
-  string input_number = "";
-  cin >> input_number;
+  string user_input_number = "";
+  cin >> user_input_number;
 
-  //We create an object to represent the roman number:
-  Roman_number number_to_convert(input_number);
+  //Make object:
+  Roman_number user_number_object = Roman_number(user_input_number);
 
-  int roman_int = number_to_convert.romanToInt();
-  cout << roman_int << "\n";
+  int test = 0;
+  test = user_number_object.roman_to_int();
+  cout << test << "\n";
+  return test;
   };
